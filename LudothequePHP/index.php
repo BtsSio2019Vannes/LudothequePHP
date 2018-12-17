5a3203029d39fdc765f82bfbfb4a6cc1e7a4abb5
admin <admin@172.16.120.69>
2018-12-17 17:08:27 +0100

    MaJ

--- LudothequePHP/index.php
+++ LudothequePHP/index.php
@@ -1,2 +1,30 @@
-<?php
-/*commentaire*/
+<!DOCTYPE html>
+
+<html>
+
+    <head>
+        <meta charset="utf-8" />
+        <link rel="stylesheet" type="text/css" href="style.css">
+   		<title><?php /* Afficher titre de la page */ ?></title>
+	</head>
+    
+    
+    <body>
+    
+        <nav>
+            <ul>
+                <li><a href="index.php?page=adherents">Gérer les Adhérents</a></li>
+                <li><a href="jeux.php">Gérer les Jeux</a></li>
+                <li><a href="emprunts.php">Gérer les Emprunts</a></li>
+	            <li><a href="parametres.php">Gérer Ludothèque</a></li>
+            </ul>
+        </nav>
+  
+        <section><!-- Méthode pour insérer chaque page -->
+            <h1><?php /* Afficher Titre de la page */ ?></h1>
+            <p><?php /* Afficher Contenu de la page */ ?></p>
+    	</section>
+      
+    </body>
+
+</html>
