74bc02b81502c5272667ba27b170c77c6fdedcc7
admin <admin@172.16.120.50>
2019-01-08 10:10:27 +0100

    Ajout CoordonnneesDAO

--- LudothequePHP/Final/db/Daos.php
+++ LudothequePHP/Final/db/Daos.php
@@ -1,36 +1,36 @@
 <?php
 namespace DAO
 {
-    
+
     use DB\Connexion\Connexion;
     include ("Connexion.php");
-    
+
     abstract class DAO
     {
-        
+
         abstract function read($id);
-        
+
         abstract function update($objet);
-        
+
         abstract function delete($objet);
-        
+
         abstract function create($objet);
-        
+
         protected $key;
-        
+
         protected $table;
-        
+
         function __construct($key, $table)
         {
             $this->key = $key;
             $this->table = $table;
             // echo "constructeur de DAO ", __NAMESPACE__,"<br/>";
         }
-        
+
         function getLastKey()
         {
             return Connexion::getInstance()->lastInsertId();
-            
+
             /*
              * Version Ã  la main qui rÃ©cupÃ¨re le max de la clÃ©, qui n'assure pas que ce soit la bonne clÃ© !
              * $sql = "SELECT Max($this->key) as max FROM $this->table";
@@ -49,17 +49,17 @@ namespace DAO\Personne
 {
 
     use Connexion\Connexion;
-use DAO;
-                                    
+    use DAO;
+
     class PersonneDAO extends \DAO\DAO
     {
-        
+
         function __construct()
         {
             parent::__construct("idPersonne", "personne");
             // echo "constructeur de DAO ", __NAMESPACE__,"<br/>";
         }
-        
+
         public function read($idPersonne)
         {
             // On utilise le prepared statemet qui simplifie les typages
@@ -67,7 +67,7 @@ use DAO;
             $stmt = Connexion::getInstance()->prepare($sql);
             $stmt->bindParam(':idPersonne', $idPersonne);
             $stmt->execute();
-            
+
             $row = $stmt->fetch();
             $idPersonne = $row["idPersonne"];
             $nom = $row["nom"];
@@ -76,14 +76,14 @@ use DAO;
             $idCoordonnees = $row["idCoordonnees"];
             $mel = $row["mel"];
             $numeroTelephone = $row["numeroTelephone"];
-            
+
             $daoPersonne = new \DAO\Personne\PersonneDAO();
             $idAdherent = $daoPersonne->retrouverAdherentAssocie($idPersonne);
-            
+
             $rep = new \Personne\Personne($idPersonne, $nom, $prenom, $dateNaissance, $idCoordonnees, $mel, $numeroTelephone, $idAdherent);
             return $rep;
         }
-        
+
         public function update($objet)
         {
             // On utilise le prepared statemet qui simplifie les typages
@@ -105,7 +105,7 @@ use DAO;
             $stmt->bindParam(':numeroTelephone', $numeroTelephone);
             $stmt->execute();
         }
-        
+
         public function delete($objet)
         {
             $sql = "DELETE FROM $this->table WHERE $this->key=:idPersonne";
@@ -113,11 +113,11 @@ use DAO;
             $idPersonne = $objet->getIdPersonne();
             $stmt->bindParam(':idPersonne', $idPersonne);
             $stmt->execute();
-            
+
             $daoPersonne = new \DAO\Personne\PersonneDAO();
             $daoPersonne->supprimerBeneficiaire($idPersonne);
         }
-        
+
         public function create($objet)
         {
             $sql = "INSERT INTO $this->table (nom,prenom,dateNaissance,idCoordonnees,mel,numeroTelephone) VALUES (:nom, :prenom, :dateNaissance, :idCoordonnees, :mel, :numeroTelephone)";
@@ -137,7 +137,7 @@ use DAO;
             $stmt->execute();
             $objet->setIdPersonne(parent::getLastKey());
         }
-        
+
         static function getPersonnes()
         {
             $sql = "SELECT * FROM personne";
@@ -153,19 +153,19 @@ use DAO;
 }
 namespace DAO\Adherent
 {
-    
+
     use DB\Connexion\Connexion;
-use DAO;
-                    
+    use DAO;
+
     class AdherentDAO extends \DAO\DAO
     {
-        
+
         function __construct()
         {
             parent::__construct("idAdherent", "adherent");
             // echo "constructeur de DAO ", __NAMESPACE__,"<br/>";
         }
-        
+
         public function read($idAdherent)
         {
             // On utilise le prepared statemet qui simplifie les typages
@@ -173,19 +173,19 @@ use DAO;
             $stmt = Connexion::getInstance()->prepare($sql);
             $stmt->bindParam(':idAdherent', $idAdherent);
             $stmt->execute();
-            
+
             $row = $stmt->fetch();
             $idAdherent = $row["idAdherent"];
             $idReglement = $row["idReglement"];
             $datePremiereAdhesion = $row["datePremiereAdhesion"];
             $dateFinAdhesion = $row["dateFinAdhesion"];
             $valeurCaution = $row["valeurCaution"];
-            
+
             $adherent = new \Personne\Adherent\Adherent($idReglement, $datePremiereAdhesion, $dateFinAdhesion, $valeurCaution);
-            
+
             $daoPersonne = new \DAO\Personne\PersonneDAO();
             $personne = $daoPersonne->read($idAdherent);
-            
+
             $adherent->setIdPersonne($personne->getIdPersonne());
             $adherent->setNom($personne->getNom());
             $adherent->setPrenom($personne->getPrenom());
@@ -193,10 +193,10 @@ use DAO;
             $adherent->setIdCoordonnees($personne->getIdCoordonnees());
             $adherent->setMel($personne->getMel());
             $adherent->setNumeroTelephone($personne->getNumeroTelephone());
-            
+
             return $adherent;
         }
-        
+
         public function update($objet)
         {
             // On utilise le prepared statemet qui simplifie les typages
@@ -213,27 +213,27 @@ use DAO;
             $stmt->bindParam(':dateFinAdhesion', $dateFinAdhesion);
             $stmt->bindParam(':valeurCaution', $valeurCaution);
             $stmt->execute();
-            
+
             $daoPersonne = new \DAO\Personne\PersonneDAO();
             $daoPersonne->update($objet);
         }
-        
+
         public function delete($objet)
         {
             $daoAdherent = new \DAO\Adherent\AdherentDAO();
             $idAdherent = $objet->getIdPersonne();
             $daoAdherent->supprimerAdherentEtBeneficiaire($idAdherent);
-            
+
             $sql = "DELETE FROM $this->table WHERE $this->key=:idAdherent";
             $stmt = Connexion::getInstance()->prepare($sql);
             $idAdherent = $objet->getIdPersonne();
             $stmt->bindParam(':idAdherent', $idAdherent);
             $stmt->execute();
-            
+
             $daoPersonne = new \DAO\Personne\PersonneDAO();
             $daoPersonne->delete($objet);
         }
-        
+
         public function create($objet)
         {
             $sql = "INSERT INTO $this->table (idAdherent,idReglement,datePremiereAdhesion,dateFinAdhesion,valeurCaution) VALUES (:idAdherent, :idReglement, :datePremiereAdhesion, :dateFinAdhesion, :valeurCaution)";
@@ -250,7 +250,7 @@ use DAO;
             $stmt->bindParam(':valeurCaution', $valeurCaution);
             $stmt->execute();
         }
-        
+
         static function getAdherents()
         {
             $sql = "SELECT * FROM personne, adherent WHERE idPersonne = idAdherent";
@@ -264,4 +264,80 @@ use DAO;
         }
     }
 }
+namespace DAO\Coordonnees
+{
+
+    use DB\Connexion\Connexion;
+    use DAO;
+
+    class CoordonneesDAO extends \DAO\DAO
+    {
+
+        function __construct()
+        {
+            parent::__construct("idCoordonnees", "coordonnees");
+            // echo "constructeur de DAO ", __NAMESPACE__,"<br/>";
+        }
+
+        public function read($idCoordonnees)
+        {
+            // On utilise le prepared statemet qui simplifie les typages
+            $sql = "SELECT * FROM $this->table WHERE $this->key=:idCoordonnees";
+            $stmt = Connexion::getInstance()->prepare($sql);
+            $stmt->bindParam(':idCoordonnees', $idCoordonnees);
+            $stmt->execute();
+
+            $row = $stmt->fetch();
+            $idCoordonnees = $row["idCoordonnees"];
+            $adresse = $row["adresse"];
+            $codePostal = $row["codePostal"];
+            $ville = $row["ville"];
+
+            $coordonnees = new \Coordonnees\Coordonnees($idCoordonnees, $adresse, $codePostal, $ville);
+
+            return $coordonnees;
+        }
+
+        public function update($objet)
+        {
+            // On utilise le prepared statemet qui simplifie les typages
+            $sql = "UPDATE $this->table SET idCoordonnees = :idCoordonnees, adresse = :adresse, codePostal = :codePostal, ville = :ville WHERE $this->key=:idCoordonnees";
+            $stmt = Connexion::getInstance()->prepare($sql);
+            $idCoordonnees = $objet->getIdCoordonnees();
+            $adresse = $objet->getAdresse();
+            $codePostal = $objet->getCodePostal();
+            $ville = $objet->getVille();
+            $stmt->bindParam(':idCoordonnees', $idCoordonnees);
+            $stmt->bindParam(':adresse', $adresse);
+            $stmt->bindParam(':codePostal', $codePostal);
+            $stmt->bindParam(':ville', $ville);
+            $stmt->execute();
+        }
+
+        public function delete($objet)
+        {
+            $sql = "DELETE FROM $this->table WHERE $this->key=:idAdherent";
+            $stmt = Connexion::getInstance()->prepare($sql);
+            $idCoordonnees = $objet->getIdCoordonnees();
+            $stmt->bindParam(':idCoordonnees', $idCoordonnees);
+            $stmt->execute();
+        }
+
+        public function create($objet)
+        {
+            $sql = "INSERT INTO $this->table (idCoordonnees,adresse,codePostal,ville) VALUES (:idCoordonnees, :adresse, :codePostal, :ville)";
+            $stmt = Connexion::getInstance()->prepare($sql);
+            $idCoordonnees = $objet->getIdCoordonnees();
+            $adresse = $objet->getAdresse();
+            $codePostal = $objet->getCodePostal();
+            $ville = $objet->getVille();
+            $stmt->bindParam(':idCoordonnees', $idCoordonnees);
+            $stmt->bindParam(':adresse', $adresse);
+            $stmt->bindParam(':codePostal', $codePostal);
+            $stmt->bindParam(':ville', $ville);
+            $stmt->execute();
+        }
+    }
+}
+
 ?>
