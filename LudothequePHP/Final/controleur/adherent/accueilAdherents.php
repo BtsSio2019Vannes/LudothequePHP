fd018182a3f985db81f8638d1293dddae92d1db1
admin <admin@172.16.120.50>
2019-01-09 09:56:23 +0100

    maj

--- LudothequePHP/Final/controleur/adherent/accueilAdherents.php
+++ LudothequePHP/Final/controleur/adherent/accueilAdherents.php
@@ -1,4 +1,11 @@
 <section>
 		<h1>Gérer les Adhérents</h1>
+
+<?php
+/*
+ * Traitement des demandes affichage (GET) et des envois de formulaire (POST)
+ * en relation avec le métier
+ */
+?>
 		
 </section>
