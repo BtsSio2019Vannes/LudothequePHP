54b2aeda4c91c766278265dd8703e241f61c6736
admin <admin@BTSWIN7-99>
2019-01-23 12:27:40 +0100

    maj du métier adherents.php / ajout du construc::parent

--- LudothequePHP/XFinal/metier/adherents.php
+++ LudothequePHP/XFinal/metier/adherents.php
@@ -1,23 +1,24 @@
 <?php
 namespace Adherent
 {
-           
+    
     class Personne
     {
+        
         private $idPersonne;
-
+        
         private $nom;
-
+        
         private $prenom;
-
+        
         private $dateNaissance;
-
+        
         private $numeroTelephone;
-
+        
         private $mel;
-
+        
         private $coordonnees;
-
+        
         function __construct($nom, $prenom, $dateNaissance, $numeroTelephone, $mel, $coordonnees)
         {
             $this->nom = $nom;
@@ -25,143 +26,153 @@ namespace Adherent
             $this->dateNaissance = $dateNaissance;
             $this->numeroTelephone = $numeroTelephone;
             $this->mel = $mel;
-            $this->coordonnees = $coordonnees;  
+            $this->coordonnees = $coordonnees;
         }
         
         public function getIdPersonne()
         {
             return $this->idPersonne;
         }
-    
+        
         public function getNom()
         {
             return $this->nom;
         }
-    
+        
         public function getPrenom()
         {
             return $this->prenom;
         }
-    
+        
         public function getDateNaissance()
         {
             return $this->dateNaissance;
         }
-    
+        
         public function getNumeroTelephone()
         {
             return $this->numeroTelephone;
         }
-    
+        
         public function getMel()
         {
             return $this->mel;
         }
-    
+        
         public function getCoordonnees()
         {
             return $this->coordonnees;
         }
-    
+        
         public function setIdPersonne($idPersonne)
         {
             $this->idPersonne = $idPersonne;
         }
-    
+        
         public function setNom($nom)
         {
             $this->nom = $nom;
         }
-    
+        
         public function setPrenom($prenom)
         {
             $this->prenom = $prenom;
         }
-    
+        
         public function setDateNaissance($dateNaissance)
         {
             $this->dateNaissance = $dateNaissance;
         }
-    
+        
         public function setNumeroTelephone($numeroTelephone)
         {
             $this->numeroTelephone = $numeroTelephone;
         }
-    
+        
         public function setMel($mel)
         {
             $this->mel = $mel;
         }
-    
+        
         public function setCoordonnees($coordonnees)
         {
             $this->coordonnees = $coordonnees;
         }
-    
+        
+        function __toString()
+        {
+            $rep = "<table class=\"personne\"><tr><td> $this->nom </td><td> $this->prenom </td><td>
+            $this->dateNaissance </td><td> $this->mel </td><td> $this->coordonnees
+            </td></tr></table>";
+            return $rep;
+        }
     }
-
+    
     class Adherent extends Personne
     {
-
+        
         private $idReglement;
-
+        
         private $dateAdhesion;
-
-        // private $date_creation_groupe;
+        
         private $dateFinAdhesion;
-
-
-        function __construct($idReglement, $dateAdhesion, $dateFinAdhesion)
+        
+        function __construct($nom, $prenom, $dateNaissance, $numeroTelephone, $mel, $coordonnees, $idReglement, $dateAdhesion, $dateFinAdhesion)
         {
+            parent::__construct($nom, $prenom, $dateNaissance, $numeroTelephone, $mel, $coordonnees);
             $this->idReglement = $idReglement;
             $this->dateAdhesion = $dateAdhesion;
             $this->dateFinAdhesion = $dateFinAdhesion;
-           
         }
         
         public function getIdReglement()
         {
             return $this->idReglement;
         }
-    
+        
         public function getDateAdhesion()
         {
             return $this->dateAdhesion;
         }
-    
+        
         public function getDateFinAdhesion()
         {
             return $this->dateFinAdhesion;
         }
-    
+        
         public function setIdReglement($idReglement)
         {
             $this->idReglement = $idReglement;
         }
-    
+        
         public function setDateAdhesion($dateAdhesion)
         {
             $this->dateAdhesion = $dateAdhesion;
         }
-    
+        
         public function setDateFinAdhesion($dateFinAdhesion)
         {
             $this->dateFinAdhesion = $dateFinAdhesion;
         }
-    
+        
+        function __toString()
+        {
+            $rep = parent::__toString() . $this->dateAdhesion . "</td><td>" . $this->dateFinAdhesion . "</td></tr></table>";
+            return $rep;
+        }
     }
-
+    
     class Coordonnees
     {
-
+        
         private $idCoordonnees;
-
+        
         private $rue;
-
+        
         private $codePostal;
-
+        
         private $ville;
-
+        
         function __construct($idCoordonnees, $rue, $codePostal, $ville)
         {
             $this->idCoordonnees = $idCoordonnees;
@@ -169,12 +180,12 @@ namespace Adherent
             $this->codePostal = $codePostal;
             $this->ville = $ville;
         }
-
+        
         public function getIdCoordonnees()
         {
             return $this->idCoordonnees;
         }
-
+        
         public function getRue()
         {
             return $this->rue;
@@ -194,20 +205,26 @@ namespace Adherent
         {
             $this->idCoordonnees = $idCoordonnees;
         }
-
+        
         public function setRue($rue)
         {
             $this->rue = $rue;
         }
-
+        
         public function setCodePostal($codePostal)
         {
             $this->codePostal = $codePostal;
         }
-
+        
         public function setVille($ville)
         {
             $this->ville = $ville;
+        }
+        
+        function __toString()
+        {
+            $rep = $this->rue . " " . $this->codePostal . " " . $this->ville;
+            return $rep;
         }
     }
 }
