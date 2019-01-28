d5898b76f717bfa95c3715135b00899f0af022f9
admin <admin@admin-PC.561557n.ac-rennes.fr>
2019-01-28 14:38:07 +0100

    maj

--- LudothequePHP/XFinal/vue/jeux/formulaireJeux.php
+++ LudothequePHP/XFinal/vue/jeux/formulaireJeux.php
@@ -34,7 +34,7 @@ function afficherJeu()
     $jeux = JeuDAO::getJeu();
     foreach ($jeux as $jeu) {
         $rep .= "<tr><td>" . $jeu->getIdJeu();
-        $rep .= "</td><td>" . $jeu->getIdRegle();
+        $rep .= "</td><td><a href=\"" . $jeu->getIdRegle() . "\">Lien</a>";
         $rep .= "</td><td>" . $jeu->getTitre();
         $rep .= "</td><td>" . $jeu->getAnneeSortie();
         $rep .= "</td><td>" . $jeu->getAuteur();
