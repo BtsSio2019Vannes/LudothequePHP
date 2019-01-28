d9f6a2100d50f7b50e66bbec9382010599f1257c
admin <admin@admin-PC.561557n.ac-rennes.fr>
2019-01-28 15:38:48 +0100

    maj

--- LudothequePHP/XFinal/vue/jeux/formulaireJeux.php
+++ LudothequePHP/XFinal/vue/jeux/formulaireJeux.php
@@ -5,7 +5,7 @@ function afficherJeu()
 {
     ?>
 <h1>Gérér les Jeux</h1>
-<form action="../index.php?page=jeux" method="post" class="AfficheJeu">
+<form action="index.php?page=jeux" method="post" class="AfficheJeu">
 	<table style="width: 90%">
 		<tr>
 			<th><input type="submit" name="mise a jour" value="Mettre à jour"></th>
@@ -57,7 +57,7 @@ function formulaireMaj($jeu)
 {
     ?>
 
-<form class="formulaireMaj" method="post" action="../controleur/accueilJeux.php">
+<form class="formulaireMaj" method="post" action="index.php?page=jeux">
     <?php
 
     ?>
@@ -134,7 +134,7 @@ function formulaireMaj($jeu)
 function afficherFomulaireAjout($jeu)
 {
     ?>
-<form method="post" action="../controleur/accueilJeux.php" class="majJeu">
+<form method="post" action="index.php?page=jeux" class="majJeu">
 	<table>
 		<tr>
 
