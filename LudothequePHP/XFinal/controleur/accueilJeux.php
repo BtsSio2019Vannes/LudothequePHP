43a6a6b4037e18a39590e182e4727e3b7bcf5ece
admin <admin@admin-PC.561557n.ac-rennes.fr>
2019-01-28 09:30:11 +0100

    maj jeu

--- LudothequePHP/XFinal/controleur/accueilJeux.php
+++ LudothequePHP/XFinal/controleur/accueilJeux.php
@@ -1,11 +1,65 @@
+<?php
+use Jeu\Jeu;
+use DAO\Jeu\JeuDAO;
+?>
 <section>
 		<h1>Gérer les Jeux</h1>
 
 <?php
-/*
- * Traitement des demandes affichage (GET) et des envois de formulaire (POST)
- * en relation avec le métier
- */
+if (htmlspecialchars(isset($_POST['ajouter un jeu']))) {
+    
+    $idRegle = htmlspecialchars($_POST['idRegle']);
+    $titre = htmlspecialchars($_POST['titre']);
+    $anneeSortie = htmlspecialchars($_POST['anneSortie']);
+    $auteur = htmlspecialchars($_POST['auteur']);
+    $idEditeur = htmlspecialchars($_POST['idEditeur']);
+    $categorie = htmlspecialchars($_POST['categorie']);
+    $univers = htmlspecialchars($_POST['univers']);
+    $contenuInitial = htmlspecialchars($_POST['contenuInitial']);
+    
+    $jeu = new Jeu("", $idRegle, $titre, $anneeSortie, $auteur, $idEditeur, $categorie, $univers, $contenuInitial);
+    $dao = new JeuDAO();
+    $dao->create($jeu);
+    
+    $messageErreur = "Erreur";
+    $idRegle = isset($idRegle) && $idRegle != " " ? $idRegle : $messageErreur;
+    $titre = isset($titre) && $titre != "" ? $titre : $messageErreur;
+    $anneeSortie = isset($anneeSortie) && $anneeSortie != "" ? $anneeSortie : $messageErreur;
+    $auteur = isset($auteur) && $auteur != "" ? $auteur : $messageErreur;
+    $idEditeur = isset($idEditeur) && $idEditeur != "" ? $idEditeur : $messageErreur;
+    $categorie = isset($categorie) && $categorie != "" ? $categorie : $messageErreur;
+    $univers = isset($univers) && $univers != "" ? $univers : $messageErreur;
+    $contenuInitial = isset($contenuInitial) && $contenuInitial != "" ? $contenuInitial : $messageErreur;
+}
+elseif (htmlspecialchars(isset($_POST['mettre à jour un jeu'])))
+{
+    $idRegle = htmlspecialchars($_POST['idRegle']);
+    $titre = htmlspecialchars($_POST['titre']);
+    $anneeSortie = htmlspecialchars($_POST['anneSortie']);
+    $auteur = htmlspecialchars($_POST['auteur']);
+    $idEditeur = htmlspecialchars($_POST['idEditeur']);
+    $categorie = htmlspecialchars($_POST['categorie']);
+    $univers = htmlspecialchars($_POST['univers']);
+    $contenuInitial = htmlspecialchars($_POST['contenuInitial']);
+    
+    $jeu = new Jeu($idJeu, $idRegle, $titre, $anneeSortie, $auteur, $idEditeur, $categorie, $univers, $contenuInitial);
+    $dao = new JeuDAO();
+    $dao->update($jeu);
+}
+elseif (htmlspecialchars(isset($_POST['supprimer un jeu'])))
+{
+    $idJeu = htmlspecialchars($_POST['idJeu']);
+    $jeu = new Jeu($idJeu, $idRegle, $titre, $anneeSortie, $auteur, $idEditeur, $categorie, $univers, $contenuInitial);
+    $jeu->setIdJeu($idJeu);
+    $dao = new JeuDAO();
+    $dao->delete($jeu);
+}
+else
+{
+    afficherJeu();
+}
+
+
 ?>
 		
 </section>
