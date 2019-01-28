392db203fc6695f26aac6df4e8dedc0fa94aab78
admin <admin@admin-PC.561557n.ac-rennes.fr>
2019-01-28 16:25:02 +0100

    maj

--- LudothequePHP/XFinal/vue/jeux/formulaireJeux.php
+++ LudothequePHP/XFinal/vue/jeux/formulaireJeux.php
@@ -1,51 +1,51 @@
 <?php
 use DAO\Jeu\JeuDAO;
 include '../controleur/accueilJeux.php';
-function afficherJeu()
-{
-    ?>
-<h1>Gérér les Jeux</h1>
-<form action="index.php?page=jeux" method="post" class="AfficheJeu">
+// function afficherJeu()
+// {
+//     ?>
+<!-- <h1>Gérér les Jeux</h1> -->
+<!-- <form action="index.php?page=jeux" method="post" class="AfficheJeu"> -->
 	<table style="width: 90%">
 		
 
-		<tr>
-			<th>Id</th>
-			<th>Règle</th>
-			<th>Titre</th>
-			<th>Année de sortie</th>
-			<th>Auteur</th>
-			<th>Id Editeur</th>
-			<th>Catégorie</th>
-			<th>Univers</th>
-			<th>Contenu Initial</th>
+<!-- 		<tr> -->
+<!-- 			<th>Id</th> -->
+<!-- 			<th>Règle</th> -->
+<!-- 			<th>Titre</th> -->
+<!-- 			<th>Année de sortie</th> -->
+<!-- 			<th>Auteur</th> -->
+<!-- 			<th>Id Editeur</th> -->
+<!-- 			<th>Catégorie</th> -->
+<!-- 			<th>Univers</th> -->
+<!-- 			<th>Contenu Initial</th> -->
 
-		</tr>
-		<tr>
-			<th colspan="8"></th>
-		</tr>
+<!-- 		</tr> -->
+<!-- 		<tr> -->
+<!-- 			<th colspan="8"></th> -->
+<!-- 		</tr> -->
 		<?php
 
-    $jeux = JeuDAO::getJeu();
-    foreach ($jeux as $jeu) {
-        $rep .= "<tr><td>" . $jeu->getIdJeu();
-        $rep .= "</td><td><a href=\"" . $jeu->getIdRegle() . "\">Règle du jeu</a>";
-        $rep .= "</td><td>" . $jeu->getTitre();
-        $rep .= "</td><td>" . $jeu->getAnneeSortie();
-        $rep .= "</td><td>" . $jeu->getAuteur();
-        $rep .= "</td><td>" . $jeu->getEditeur()->getIdEditeur();
-        $rep .= "</td><td>" . $jeu->getCategorie();
-        $rep .= "</td><td>" . $jeu->getUnivers();
-        $rep .= "</td><td>" . $jeu->getContenuInitial();
-        $rep .= "</td><td><input type=\"radio\" name=\"idJeu\ value=\"" . $jeu->getIdJeu() . "\" label for =\"idJeu\"></td></tr>";
-    }
-    echo $rep;
-}
-?>
+//     $jeux = JeuDAO::getJeu();
+//     foreach ($jeux as $jeu) {
+//         $rep .= "<tr><td>" . $jeu->getIdJeu();
+//         $rep .= "</td><td><a href=\"" . $jeu->getIdRegle() . "\">Règle du jeu</a>";
+//         $rep .= "</td><td>" . $jeu->getTitre();
+//         $rep .= "</td><td>" . $jeu->getAnneeSortie();
+//         $rep .= "</td><td>" . $jeu->getAuteur();
+//         $rep .= "</td><td>" . $jeu->getEditeur()->getIdEditeur();
+//         $rep .= "</td><td>" . $jeu->getCategorie();
+//         $rep .= "</td><td>" . $jeu->getUnivers();
+//         $rep .= "</td><td>" . $jeu->getContenuInitial();
+//         $rep .= "</td><td><input type=\"radio\" name=\"idJeu\ value=\"" . $jeu->getIdJeu() . "\" label for =\"idJeu\"></td></tr>";
+//     }
+//     echo $rep;
+// }
+// ?>
 
-	</table>
+<!-- 	</table> -->
 
-</form>
+<!-- </form> -->
 
 <?php
 
