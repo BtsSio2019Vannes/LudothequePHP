c9ed45f706bbcd9d316ba2687570203b2e46f929
Ga Quel <gaquel@hotmail.fr>
2019-04-16 15:46:11 +0200

    Maj mise en page Emprunts

--- LudothequePHP/XFinal/vue/emprunt/formulaireEmprunts.php
+++ LudothequePHP/XFinal/vue/emprunt/formulaireEmprunts.php
@@ -16,10 +16,18 @@ function afficherGestionEmprunt($listeEmprunts)
 				<td colspan="6"><button type="submit" class="btn btn-success"
 						name="nouvelEmprunt">
 						<span class="glyphicon glyphicon-plus"></span> Nouvel Emprunt
-					</button> <a href="index.php?page=emprunts&action=gererAlerte"
-					class="btn btn-info"><span
+					</button> <a href="index.php?page=emprunts&action=gererAlerte"><button class="btn btn-info"><span
 						class="glyphicon glyphicon-exclamation-sign"></span> Gérer les
-						Alertes</a></td>
+						Alertes</button></a>
+						<button type="submit" class="btn btn-danger"
+						name="supprimerEmprunt">
+						<span class="glyphicon glyphicon-remove"></span> Supprimer
+					</button>
+					<button type="submit" class="btn btn-primary"
+						name="modifierEmprunt">
+						<span class="glyphicon glyphicon-edit"></span> Mettre à Jour
+					</button>
+						</td>
 			</tr>
 			<tr>
 				<th>Date d'emprunt</th>
@@ -27,14 +35,7 @@ function afficherGestionEmprunt($listeEmprunts)
 				<th>Adhérent</th>
 				<th>Jeu</th>
 				<th>Alerte</th>
-				<th><button type="submit" class="btn btn-danger"
-						name="supprimerEmprunt">
-						<span class="glyphicon glyphicon-remove"></span> Supprimer
-					</button>
-					<button type="submit" class="btn btn-primary"
-						name="modifierEmprunt">
-						<span class="glyphicon glyphicon-edit"></span> Mettre à Jour
-					</button></th>
+				<th></th>
 			</tr>
 		</thead>
 		<tbody>
